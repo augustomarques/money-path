448b6b90db7b253046ec546ca3f586d6c6c88fc4
Augusto Marques <augusto.marques12@gmail.com>
2023-07-02 16:16:24 -0300

    first commit of money-path

---
+++ src/main/kotlin/br/com/amarques/moneypath/MoneyPathApplication.kt
@@ -0,0 +1,11 @@
+package br.com.amarques.moneypath
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoneyPathApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoneyPathApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/amarques/moneypath/MoneyPathApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.amarques.moneypath
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoneyPathApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
